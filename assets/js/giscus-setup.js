26bb5965422f2e04d2d628c14481c5199ef10ec0
satchwaldman <66792060+satchwaldman@users.noreply.github.com>
2025-09-27 20:30:58 +0000

    Deploying to gh-pages from @ satchwaldman/satchwaldman.github.io@09b45040af6ca7fa9e3651ab0f69b90ede7cea98 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "satchwaldman/personal-website",
+    "data-repo": "satchwaldman/satchwaldman.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
